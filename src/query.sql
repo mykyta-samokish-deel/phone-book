36e0f77d2da1e4f5d09b0bbc8979561758f69d4a
mykytasamokish <nikita.samokish@gmail.com>
2015-02-18 22:34:54 -0600

    add target folder to .gitignore

--- src/query.sql
+++ src/query.sql
@@ -1,1 +1,15 @@
-select * from pb_contacts;
+DROP TABLE pb_phones;
+DROP TABLE pb_users;
+
+CREATE TABLE pb_users(
+  user_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50)
+);
+
+CREATE TABLE pb_phones(
+  phone_id VARCHAR(50),
+  phone VARCHAR(50),
+  user_id SERIAL,
+  FOREIGN KEY (user_id) REFERENCES pb_users
+);
